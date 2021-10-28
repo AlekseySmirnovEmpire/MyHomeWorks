70878929a3dcd7054ccf7510b4325d43435b348f
Aleksey Smirnov <i@smirnov-alexsey.ru>
2021-10-28 20:33:32 +0300

    project init

---
+++ 13_1/13_1/13_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+s
+
+int main() {
+	std::vector<int> robots = { 15, 25, 32, 37, 42, 45, 50 };
+
+	std::cout << "How many robots: ";
+
+	
+}
