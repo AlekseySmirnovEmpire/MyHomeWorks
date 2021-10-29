826414cc6263a513994e5807c32d520d7da58dd9
Aleksey Smirnov <i@smirnov-alexsey.ru>
2021-10-29 22:36:06 +0300

    Teacher's comments, delete bubble sort

--- 12_2/12_2/12_2.cpp
+++ 12_2/12_2/12_2.cpp
@@ -8,23 +8,20 @@ int main() {
 		std::cin >> arr[i];
 	}
 
-	int temp;
-	bool find = false;
+	int sum = 0, min = arr[0], temp = 0;
 
-	for (int i = 0; i < 15 && !find; i++) {
-		for (int j = 0; j < 14; j++) {
-			if (arr[j] > arr[j + 1]) {
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
-		if (arr[i] == arr[i - 1] && (i > 0)) {
-			std::cout << "Number wrote twice: " << arr[i];
-			find = true;
-		}
-	}//Сортировка пузырьком + поиск числа
+	for (int i = 0; i < 15; i++) {
+		sum += arr[i];
 
-	/* Пузырьком делаем сортировку, потому что по условию надо как можно меньше памяти использовать*/
+		if (arr[i] < min) min = arr[i];
+	}
+
+	for (int i = 0; i < 15; i++) {
+		temp += i + min;
+	}
+
+	temp -= sum;
+
+	std::cout << "Doubling number is: " << min + (14 - temp) << std::endl;
 
 }
