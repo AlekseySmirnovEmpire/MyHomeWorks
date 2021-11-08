c980c7b9acd2a0d6e681078b4481f05fef99ca15
Aleksey Smirnov <i@smirnov-alexsey.ru>
2021-11-08 18:53:39 +0300

    init

---
+++ 16_2/16_2/16_2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+int main() {
+	/*Надо написать небольшую программу. Из целой и дробной частей числа, 
+	которые в отдельности вводит пользователь надо составить новое число 
+	с плавающей точкой (типа double) и вывести это число обратно в консоль, 
+	для проверки. Целая часть - это та, часть числа, которая находится 
+	до точки (запятой), дробная - после.*/
+
+	double firstPart;
+	double secondPart;
+	std::stringstream result;
+
+	std::cout << "Input your first part: ";
+	std::cin >> firstPart;
+
+	std::cout << "Input your second part: ";
+	std::cin >> secondPart;
+
+	while (secondPart < 0) {
+		std::cout << "Wrong input. Please, try again: ";
+		std::cin >> secondPart;
+	}
+
+	result << firstPart << '.' << secondPart;
+
+	std::cout << result.str() << std::endl;
+}
