87b955fc896b095e01d4b157969e576dcf2cc1ac
Aleksey Smirnov <i@smirnov-alexsey.ru>
2021-11-14 00:31:26 +0300

    init

---
+++ 21_1/21_1/21_1.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+struct Statesment
+{
+	std::string firstName = "unknown";
+	std::string lastName = "unknown";
+	std::string date = "01.01.1900";
+	int payment = 0;
+};
+
+bool isCorrectDate(std::string date) {
+	int day = std::stoi(date.substr(0, 2));
+	int month = std::stoi(date.substr(3, 2));
+	int year = std::stoi(date.substr(6, 4));
+
+	if ((year >= 1900 && year <= 2021) && (month > 0 && month <= 12)) {
+		if (((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+				&& day <= 31) || ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30) ||
+				(month == 2 && ((year % 4 == 0 && day <= 29) || (year % 4 != 0 && day <= 28))) && day > 0) {
+			return true;
+		}
+		else return false;
+	}
+	else return false;
+}
+
+void showHelpMenu(std::string input) {
+	std::cout << "\"/list\" - show statesment's lists;" << std::endl;
+	std::cout << "\"/add\" - add new data in statesment's lists;" << std::endl;
+	std::cout << "\"/exit\" - close programme;" << std::endl;
+}
+
+bool isCorrectInput(std::string input) {
+	return (input == "/add" || input == "/help" || input == "/exit" || input == "/list");
+}
+
+void newLineAdd(std::ofstream& file, Statesment& person) {
+	std::cout << "Input name first name: ";
+	std::getline(std::cin, person.firstName);
+
+	std::cout << "Input name last name: ";
+	std::getline(std::cin, person.lastName);
+
+	std::cout << "Input date (dd.mm.yyyy): ";
+	std::getline(std::cin, person.date);
+	while (!isCorrectDate(person.date)) {
+		std::cout << "Wrong input, try again: ";
+		std::getline(std::cin, person.date);
+	}
+
+	std::cout << "Input peayment amount: ";
+	std::cin >> person.payment;
+	std::cin.ignore();
+
+	file << person.firstName << ' ' << person.lastName << ' ' << person.date << ' ' << person.payment << std::endl;
+}
+
+int main() {
+	std::cout << "~~~~~~~~~~~~~~~~~WELCOME~~~~~~~~~~~~~~~~~~" << std::endl;
+
+	Statesment person;
+
+	
+	std::string userInput = "";
+
+	while (userInput != "/exit") {
+		std::cout << "Write command (\"/help\" to show commands): ";
+		std::getline(std::cin, userInput);
+		while (!isCorrectInput(userInput)) {
+			std::cout << "Wrong command, try again: ";
+			std::getline(std::cin, userInput);
+		}
+
+		while (userInput == "/help") {
+			showHelpMenu(userInput);
+			std::cout << "Write command: ";
+			std::getline(std::cin, userInput);
+		}
+
+		if (userInput == "/list") {
+			std::ifstream file("D:\\Alex\\SkillBox_HomeWorks\\MyHomeWorks\\21_1\\21_1\\statements.txt");
+
+			while (!file.eof()) {
+				file >> person.firstName >> person.lastName >> person.date >> person.payment;
+				std::cout << person.firstName << ' ' << person.lastName << ' ' << person.date << ' ' << person.payment << std::endl;
+			}
+			
+			file.close();
+		}
+		else if (userInput == "/add") {
+			std::ofstream file("D:\\Alex\\SkillBox_HomeWorks\\MyHomeWorks\\21_1\\21_1\\statements.txt", std::ios::app);
+			newLineAdd(file, person);
+			file.close();
+		}
+	}
+}
---
+++ 21_2/21_2/21_2.cpp
@@ -0,0 +1,294 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+enum eBuildingTypes {
+	GARAGE = 1,
+	BARN,
+	BATHHOUSE
+};
+
+enum eRoomsType {
+	NON,
+	BEDROOM,
+	KITCHEN,
+	BATHROOM,
+	CHILDRENROOM,
+	MEETINGROOM
+};
+
+struct Garage
+{
+	bool isFound = false;
+	double square = 0;
+};
+
+struct Barn
+{
+	bool isFound = false;
+	double square = 0;
+};
+
+struct BathHouse
+{
+	bool isFound = false;
+	bool isStove = false;
+	double square = 0;
+};
+
+struct Room
+{
+	double square = 0;
+};
+
+struct Floor
+{
+	double height = 0;
+	std::vector<Room> room;
+};
+
+struct House
+{
+	bool isStove = false;
+	std::vector<Floor> floor;
+};
+
+struct Region
+{
+	House home;
+	BathHouse bathhouseStruct;
+	Garage garageStruct;
+	Barn barnStruck;
+};
+
+void inputBuilding(Region& reg, int* counter) {
+	std::cout << "Whats' the types of building? Type:" << std::endl;
+	std::cout << "\"" << GARAGE <<"\" for garage;" << std::endl;
+	std::cout << "\"" << BARN << "\" for barn;" << std::endl;
+	std::cout << "\"" << BATHHOUSE << "\" for bathhouse;" << std::endl;
+
+	int answer;
+	std::cin >> answer;
+	std::cin.ignore();
+
+	while (answer < GARAGE || answer > BATHHOUSE) {
+		std::cout << "Wrong! Try again: ";
+		std::cin >> answer;
+		std::cin.ignore();
+	}
+
+	if (answer == GARAGE && !reg.garageStruct.isFound) {
+		reg.garageStruct.isFound = true;
+
+		std::cout << "What's the square of garage: ";
+		std::cin >> reg.garageStruct.square;
+		std::cin.ignore();
+
+		(*counter)--;
+
+		std::cout << "------------------------------------" << std::endl;
+	}
+	else if (answer == BARN && !reg.barnStruck.isFound) {
+		reg.barnStruck.isFound = true;
+
+		std::cout << "What's the square of barn: ";
+		std::cin >> reg.barnStruck.square;
+		std::cin.ignore();
+
+		(*counter)--;
+
+		std::cout << "------------------------------------" << std::endl;
+	}
+	else if (answer == BATHHOUSE && !reg.bathhouseStruct.isFound) {
+		reg.bathhouseStruct.isFound = true;
+
+		std::cout << "Have bathhouse any stove? (y/n) ";
+		char answer;
+		std::cin >> answer;
+		std::cin.ignore();
+
+		reg.bathhouseStruct.isStove = answer == 'y';
+
+		std::cout << "What's the square of bathhouse: ";
+		std::cin >> reg.bathhouseStruct.square;
+		std::cin.ignore();
+
+		(*counter)--;
+
+		std::cout << "------------------------------------" << std::endl;
+	}
+	else {
+		std::cout << "Wrong, you have already write this. Try again!" << std::endl;
+	}
+}
+
+void roomType(Room& rooms) {
+	
+	std::cout << "What's the square of room: ";
+	std::cin >> rooms.square;
+	std::cin.ignore();
+
+}
+
+void inputRoom(Room& rooms) {
+	std::cout << "What's the type of room? Type:" << std::endl;
+	std::cout << "\"" << BEDROOM << "\" for bedroom;" << std::endl;
+	std::cout << "\"" << KITCHEN << "\" for kitchen;" << std::endl;
+	std::cout << "\"" << BATHROOM << "\" for bathroom;" << std::endl;
+	std::cout << "\"" << CHILDRENROOM << "\" for children room;" << std::endl;
+	std::cout << "\"" << MEETINGROOM << "\" for meetingroom;" << std::endl;
+
+	int answer;
+	std::cin >> answer;
+	std::cin.ignore();
+
+	while (answer < BEDROOM || answer > MEETINGROOM) {
+		std::cout << "Wrong input, try again: ";
+		std::cin >> answer;
+		std::cin.ignore();
+	}
+
+	if (answer == BEDROOM) {
+		std::cout << "~~~~~~~~~~~~~~BEDROOM~~~~~~~~~~~~~~" << std::endl;
+
+		roomType(rooms);
+	}
+	else if (answer == KITCHEN) {
+		std::cout << "~~~~~~~~~~~~~~KITCHEN~~~~~~~~~~~~~~" << std::endl;
+
+		roomType(rooms);
+	}
+	else if (answer == BATHROOM) {
+		std::cout << "~~~~~~~~~~~~~~BATHROOM~~~~~~~~~~~~~~" << std::endl;
+
+		roomType(rooms);
+	}
+	else if (answer == CHILDRENROOM) {
+		std::cout << "~~~~~~~~~~~~~~CHILDRENROOM~~~~~~~~~~~~~~" << std::endl;
+
+		roomType(rooms);
+	}
+	else if (answer == MEETINGROOM) {
+		std::cout << "~~~~~~~~~~~~~~MEETINGROOM~~~~~~~~~~~~~~" << std::endl;
+
+		roomType(rooms);
+	}
+
+	std::cout << "-----------------------------------" << std::endl;
+}
+
+void inputFloor(Floor& floor) {
+	std::cout << "Input height of the floor: ";
+	std::cin >> floor.height;
+	std::cin.ignore();
+
+	std::cout << "How many rooms on this floor: ";
+	int roomsCounter;
+	std::cin >> roomsCounter;
+	std::cin.ignore();
+
+	while (roomsCounter < 1) {
+		std::cout << "Wrong, try again: ";
+		std::cin >> roomsCounter;
+		std::cin.ignore();
+	}
+
+	floor.room.resize(roomsCounter);
+
+	for (int i = 0; i < roomsCounter; i++) {
+		std::cout << "---------------" << i + 1 << " ROOM---------------" << std::endl;
+		
+		inputRoom(floor.room[i]);
+	}
+
+	std::cout << "-------------------------------" << std::endl;
+}
+
+void inputHouse(House& home) {
+	std::cout << "Have house any stove? (y/n) ";
+	char answer;
+	std::cin >> answer;
+	std::cin.ignore();
+	home.isStove = answer == 'y';
+
+	std::cout << "How many floors in the house: ";
+	int floorsCounter;
+	std::cin >> floorsCounter;
+	std::cin.ignore();
+
+	while (floorsCounter < 1) {
+		std::cout << "Wrong, try again: ";
+		std::cin >> floorsCounter;
+		std::cin.ignore();
+	}
+
+	home.floor.resize(floorsCounter);
+
+	for (int i = 0; i < floorsCounter; i++) {
+		std::cout << "==============" << i + 1 << " FLOOR==============" << std::endl;
+		
+		inputFloor(home.floor[i]);
+	}
+	
+	std::cout << "-------------------------------------------" << std::endl;
+}
+
+void inputRegion(std::vector<Region>& regions, int count) {
+	for (int i = 0; i < count; i++) {
+
+		std::cout << "~~~~~~~~~~~~~~~~~" << i + 1 << " REGION~~~~~~~~~~~~~~~~~~~~" << std::endl;
+
+		std::cout << "How many buildings except house is on area: ";
+		int housesCounter;
+		std::cin >> housesCounter;
+		std::cin.ignore();
+		
+		std::cout << "---------------INPUT HOUSE---------------" << std::endl;
+
+		inputHouse(regions[i].home);
+
+		while (housesCounter > 0) {
+			std::cout << "-----------------INPUT OTHER BUILDINGS-----------------" << std::endl;
+
+			inputBuilding(regions[i], &housesCounter);
+		}
+	}
+}
+
+double totalSquare(std::vector<Region>& reg) {
+
+	double temp = 0;
+	
+	for (int i = 0; i < reg.size(); i++) {
+
+		temp += reg[i].barnStruck.square + reg[i].bathhouseStruct.square + reg[i].garageStruct.square;
+
+		for (int j = 0; j < reg[i].home.floor.size(); j++) {
+			for (int k = 0; k < reg[i].home.floor[j].room.size(); k++) {
+				temp += reg[i].home.floor[j].room[k].square;
+			}
+		}
+	}
+
+	return temp;
+	
+}
+
+int main() {
+
+	std::cout << "~~~~~~~~~~~~~~~~~~WELCOME!~~~~~~~~~~~~~~~~~~" << std::endl;
+
+	int regionsCount;
+	std::cout << "Input amount of regions in village: ";
+	std::cin >> regionsCount;
+	std::cin.ignore();
+
+	std::vector<Region> regions(regionsCount);
+
+	inputRegion(regions, regionsCount);
+
+	std::cout << "Total square = " << totalSquare(regions) << std::endl;
+
+	std::cout << "~~~~~~~~~~~~~~~~~~GOODBYE!~~~~~~~~~~~~~~~~~~" << std::endl;
+}
