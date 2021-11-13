d927be56a27c6dcd24fb991777c6624feaec9d3d
Aleksey Smirnov <i@smirnov-alexsey.ru>
2021-11-14 00:32:51 +0300

    correct date search

--- 20_1/20_1/20_1.cpp
+++ 20_1/20_1/20_1.cpp
@@ -9,8 +9,8 @@ bool isDateCorrect(std::string date) {
 
 	if ((year > 1900 && year < 2021) && (month > 0 && month <= 12)) {
 		if (((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-				&& day == 31) || ((month == 4 || month == 6 || month == 9 || month == 11) && day == 30) || 
-				(month == 2 && ((year % 4 == 0 && day == 29) || (year % 4 != 0 && day == 28)))) {
+				&& day <= 31) || ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30) || 
+				(month == 2 && ((year % 4 == 0 && day <= 29) || (year % 4 != 0 && day <= 28))) && day > 0) {
 			return true;
 		}
 		else {
