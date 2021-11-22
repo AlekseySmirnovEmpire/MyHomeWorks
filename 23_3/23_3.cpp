9866a1e78f3f6d1c2ea21aa97c23f4f9d96fc812
Aleksey Smirnov <i@smirnov-alexsey.ru>
2021-11-23 00:31:53 +0300

    init

---
+++ 23_3/23_3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+//#define SPRING
+
+#define AUTUMN
+
+//#define SUMMER
+
+//#define WINTER
+
+int main() {
+
+#ifdef SPRING
+	std::cout << "SPRING" << std::endl;
+#endif
+
+#ifdef SUMMER
+	std::cout << "SUMMER" << std::endl;
+#endif 
+
+#ifdef AUTUMN
+	std::cout << "AUTUMN" << std::endl;
+#endif 
+
+#ifdef WINTER
+	std::cout << "WINTER" << std::endl;
+#endif 
+}
