3379a7319079d87adf142ed538fa17db819ccaca
Aleksey Smirnov <i@smirnov-alexsey.ru>
2021-12-06 21:26:47 +0300

    rework struck to read date with std::tm

--- 24_2/24_2.cpp
+++ 24_2/24_2.cpp
@@ -1,199 +1,91 @@
+#pragma warning(disable : 4996)
 #include <iostream>
 #include <ctime>
 #include <string>
 #include <vector>
-#include <cmath>
+#include <iomanip>
 
+enum eMonth
+{
+	JANUARY = 1,
+	FEBRUARY,
+	MARCH,
+	APRIL,
+	MAY,
+	JUNE,
+	JULY,
+	AUGUST,
+	SEPTEMBER,
+	OCTOBER,
+	NOVEMBER,
+	DECEMBER
+};
 
 struct BirthDays
 {
 	std::string name = "";
-	std::string date = "";
+	std::tm date;
 };
 
-bool isCorrectName(std::string& input) {
-	for (int i = 0; i < input.length(); ++i) {
-		if (!(input[i] >= 'a' && input[i] <= 'z') && !(input[i] >= 'A' && input[i] <= 'Z') && input[i] != ' ') return false;
-	}
+int convertToDay(std::tm& tm) {
 
-	return true;
-}
+	int tempMonth = tm.tm_mon - 1;
 
-bool isCorrectDate(std::string& date) {
+	int output = 0;
 
-	if (date.length() != 10) return false;
-
-	int year = std::stoi(date.substr(0, 4));
-	int month = std::stoi(date.substr(5, 2));
-	int day = std::stoi(date.substr(8, 2));
-
-	if ((year > 1900 && year < 2021) && (month > 0 && month <= 12)) {
-		if (((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-			&& day <= 31) || ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30) ||
-			(month == 2 && ((year % 4 == 0 && day <= 29) || (year % 4 != 0 && day <= 28))) || day > 0) {
-			return true;
+	while (tempMonth != 0) {
+		if (tempMonth == 1 || tempMonth == 3 || tempMonth == 5 || tempMonth == 7 
+				|| tempMonth == 8 || tempMonth == 10 || tempMonth == 12) {
+			output += 31;
+		}
+		else if (tempMonth == 4 || tempMonth == 6 || tempMonth == 9 || tempMonth == 11) {
+			output += 30;
+		}
+		else if (tm.tm_year % 4 == 0 && tempMonth == 2) {
+			output += 29;
 		}
 		else {
-			return false;
+			output += 28;
+		}
+
+		--tempMonth;
+	}
+
+	return output;
+}
+
+void searchNearestBirthDay(std::vector<BirthDays>& input, std::vector<BirthDays>& output) {
+
+	std::time_t start = std::time(nullptr);
+	std::tm nowTime = *std::localtime(&start);
+
+	int temp = convertToDay(nowTime);
+
+	for (int i = 0; i < input.size(); ++i) {
+		if (convertToDay(input[i].date) >= temp) {
+
+			BirthDays* tempData = new BirthDays;
+
+			tempData->name = input[i].name;
+			tempData->date = input[i].date;
+
+			if (output.size() == 0 || convertToDay(output[0].date) == convertToDay(input[i].date)) {
+
+				output.push_back(*tempData);
+			}
+			else {
+
+				if (output.size() > 1) output.resize(1);
+
+				output[0].name = tempData->name;
+
+				output[0].date = tempData->date;
+			}
+
+			delete tempData;
+			tempData = nullptr;
 		}
 	}
-	else {
-		return false;
-	}
-}
-
-void inputDataInList(std::string& name, std::string& date, std::vector<BirthDays>& origin) {
-
-	BirthDays* temp = new BirthDays;
-
-	temp->name = name;
-	temp->date = date;
-
-	origin.push_back(*temp);
-
-	delete temp;
-
-	temp = nullptr;
-}
-
-int getDaysCount(std::string& date) {
-
-	int* month = new int;
-
-	*month = std::stoi(date.substr(5, 2)) - 1;
-
-	int output = std::stoi(date.substr(8, 2));
-
-	switch (*month)
-	{
-
-	case 12:
-		output += 31;
-		--* month;
-
-	case 11:
-		output += 30;
-		--* month;
-
-	case 10:
-		output += 31;
-		--* month;
-
-	case 9:
-		output += 30;
-		--* month;
-
-	case 8:
-		output += 31;
-		--* month;
-
-	case 7:
-		output += 31;
-		--* month;
-
-	case 6:
-		output += 30;
-		--* month;
-
-	case 5:
-		output += 31;
-		--* month;
-
-	case 4:
-		output += 30;
-		--* month;
-
-	case 3:
-		output += 31;
-		--* month;
-
-	case 2:
-		if (std::stoi(date.substr(0, 4)) % 4 == 0) output += 29;
-		else output += 28;
-		--* month;
-
-	case 1:
-		output += 31;
-		--* month;
-
-	default:
-		break;
-	}
-
-	delete month;
-	month = nullptr;
-
-	return output;
-}
-
-int getDaysCount(std::tm* date) {
-
-	int* month = new int;
-
-	*month = date->tm_mon - 1;
-
-	int output = date->tm_mday;
-
-	switch (*month)
-	{
-
-	case 12:
-		output += 31;
-		--* month;
-
-	case 11:
-		output += 30;
-		--* month;
-
-	case 10:
-		output += 31;
-		--* month;
-
-	case 9:
-		output += 30;
-		--* month;
-
-	case 8:
-		output += 31;
-		--* month;
-
-	case 7:
-		output += 31;
-		--* month;
-
-	case 6:
-		output += 30;
-		--* month;
-
-	case 5:
-		output += 31;
-		--* month;
-
-	case 4:
-		output += 30;
-		--* month;
-
-	case 3:
-		output += 31;
-		--* month;
-
-	case 2:
-		if (date->tm_year % 4 == 0) output += 29;
-		else output += 28;
-		--* month;
-
-	case 1:
-		output += 31;
-		--* month;
-
-	default:
-		break;
-	}
-
-	delete month;
-	month = nullptr;
-
-	return output;
 }
 
 int main() {
@@ -203,71 +95,55 @@ int main() {
 	std::vector<BirthDays> birthdaysFriends;
 
 	while (true) {
-
-		std::string date;
-
-		std::cout << "Input your friend name: ";
+		BirthDays* temp = new BirthDays;
+		
+		std::cout << "Enter friends' name: ";
 		std::getline(std::cin, userInput);
-
-		while (!isCorrectName(userInput)) {
-			std::cout << "Wrong input! Try again: ";
-			std::getline(std::cin, userInput);
-		}
 
 		if (userInput == "end") break;
 
-		std::cout << "Input his birthday (YYYY/MM/DD): ";
-		std::getline(std::cin, date);
+		temp->name = userInput;
 
-		while (!isCorrectDate(date)) {
-			std::cout << "Wrong input! Try again: ";
-			std::getline(std::cin, date);
-		}
+		std::cout << "Enter his/her birthday in format YYYY/MM/DD: ";
+		std::cin >> std::get_time(&temp->date, "%Y/%m/%d");
+		std::cin.ignore();
 
-		inputDataInList(userInput, date, birthdaysFriends);
+		birthdaysFriends.push_back(*temp);
+
+		delete temp;
+		temp = nullptr;
 
 	}
 
-	std::cout << "=========================================" << std::endl;
+	if (birthdaysFriends.size() > 0) {
+		std::cout << "=========================================" << std::endl;
 
-	std::vector<int> tempIndexes = { 0 };
+		std::vector<BirthDays> nowDaysBirthDays;
 
-	std::time_t t = std::time(nullptr);
-	std::tm* local = std::localtime(&t);
+		searchNearestBirthDay(birthdaysFriends, nowDaysBirthDays);
 
-	int* temp = new int;
-	*temp = std::abs(getDaysCount(birthdaysFriends[0].date) - getDaysCount(local));
+		if (nowDaysBirthDays.size() > 1) {
 
-	for (int i = 1; i < birthdaysFriends.size(); ++i) {
+			std::cout << "There are not only one friend, that have nearest birthdays!" << std::endl;
 
-		if (std::abs(getDaysCount(birthdaysFriends[i].date) - getDaysCount(local)) < *temp) {
+			std::cout << "There are: " << std::endl;
 
-			if (tempIndexes.size() > 1) {
+			for (int i = 0; i < nowDaysBirthDays.size(); ++i) {
 
-				tempIndexes.resize(1);
+				std::cout << i + 1 << ". " << nowDaysBirthDays[i].name << std::endl;
 			}
-
-			tempIndexes[0] = i;
-
-			*temp = std::abs(getDaysCount(birthdaysFriends[i].date) - getDaysCount(local));
 		}
-		else if (std::abs(getDaysCount(birthdaysFriends[i].date) - getDaysCount(local)) == *temp) {
+		else if (nowDaysBirthDays.size() == 0) {
 
-			tempIndexes.push_back(i);
+			std::cout << "There is no nearest birthday in the year!" << std::endl;
+		}
+		else {
+			std::cout << nowDaysBirthDays[0].name << " have the nearest birthday!" << std::endl;
 		}
 	}
-
-	delete temp;
-	temp = nullptr;
-
-	if (tempIndexes.size() > 1) std::cout << "You've got more than one friend's birthdays in one day in nearest time!" << std::endl;
-
-	std::cout << "Nearest birthdays: " << std::endl;
-
-	for (int i = 0; i < tempIndexes.size(); ++i) {
-
-		std::cout << birthdaysFriends[tempIndexes[i]].name << " - " << birthdaysFriends[tempIndexes[i]].date << std::endl;
-	}
+	else {
+		std::cout << "There is no data!" << std::endl;
+	}	
 
 	std::cout << "===============THAT'S ALL FOLKS!====================" << std::endl;
 }
