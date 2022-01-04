7a7df0d6367a7328b2bd66aca47ad8f0b40b323f
Aleksey Smirnov <i@smirnov-alexsey.ru>
2022-01-04 15:20:39 +0300

    init

--- 32_1/main.cpp
+++ 32_1/main.cpp
@@ -1,33 +1,6 @@
 #include <iostream>
-#include <string>
-#include <fstream>
-#include "nlohmann/json.hpp"
 
-struct Records 
-{
-    std::string firstName;
-    std::string lastName;
-    int age;
-    bool married;
-};
-
-int main(int, char**) 
-{
-    Records record;
-
-    std::cin >> record.firstName >> record.lastName >> record.age >> record.married;
-
-    std::ofstream file("record.json");
-
-    nlohmann::json dict = 
-    {
-        {"First name", record.firstName},
-        {"Last name", record.lastName},
-        {"Age", record.age},
-        {"Married", record.married}
-    };
-
-    file << dict;
-    
-    file.close();
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
