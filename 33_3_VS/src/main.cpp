1491ab4cca0946cb585fb09ed41a87aa67691cd4
Aleksey Smirnov <i@smirnov-alexsey.ru>
2022-01-08 18:18:20 +0300

    rework for template work from main

--- 33_3_VS/src/main.cpp
+++ 33_3_VS/src/main.cpp
@@ -4,18 +4,41 @@
 
 int main(int, char**) 
 {
+    bool isOver = false;
     forever()
     {
         showMenu();
 
+        Reestr<int, int> red;
+
         int answer;
         std::cin >> answer;
 
-        switch(answer)
-        {
-        case ADD:
-            clear();
+        answer = static_cast<eCommands>(answer);
+
+        int key, data;
+
+        if(answer == EXIT)
             break;
+        else
+        {
+            std::cout << "Input key: ";
+            int key;
+            std::cin >> key;
+
+            if(answer == ADD)
+            {
+                std::cout << "Input data: ";
+                std::cin >> data;
+
+                red.addKey(key, data);
+            }
+            else if (answer == PRINT)
+                red.printKey(key);
+            else if (answer == REMOVE)
+                red.removeKey(key);
+            else
+                red.findKey(key);
         }
     }
 }
